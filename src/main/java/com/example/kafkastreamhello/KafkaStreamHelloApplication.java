c80a3751452abfe0d10def4a400fb484a238378c
LAP01171 <lap01171@LAP01171s-MacBook-Pro.local>
2019-07-15 15:30:25 +0700

    create a simple topology: sorce -> upper-case --> sink

---
+++ src/main/java/com/example/kafkastreamhello/KafkaStreamHelloApplication.java
@@ -0,0 +1,58 @@
+package com.example.kafkastreamhello;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+@SpringBootApplication
+public class KafkaStreamHelloApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamHelloApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "tuhucon");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        streamsBuilder.stream("src-topic", Consumed.with(Serdes.String(), Serdes.String()))
+                .mapValues(s -> {
+                    if (s == null) return null;
+                    return s.toUpperCase();
+                }).peek((s, s2) -> System.out.println(s + ": " + s2))
+                .to("des-topic", Produced.with(Serdes.String(), Serdes.String()));
+
+        Topology topo = streamsBuilder.build();
+        System.out.println(topo.describe());
+
+        KafkaStreams kafkaStreams = new KafkaStreams(topo, properties);
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            kafkaStreams.close();
+            countDownLatch.countDown();
+        }));
+
+        try {
+            kafkaStreams.start();
+            countDownLatch.await();
+        } catch (Exception ex) {
+            kafkaStreams.close();
+        }
+    }
+}
